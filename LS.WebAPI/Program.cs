6d532934eacce5d9d63234905757c05eef720ca0
MohitNegi <dcsmohitnegi@outlook.com>
2026-07-26 17:37:17 +0530

    Updated Web API

--- LS.WebAPI/Program.cs
+++ LS.WebAPI/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
             .WithOrigins(
                 "http://localhost:5173",
                 "http://localhost:3000",
-                "https://legal-nine-sooty.vercel.app",
+                "https://legal-nine-sooty.vercel.app"
                 
             )
             .AllowAnyHeader()
